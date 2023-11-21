669f5adf1cd8a8bf67c2dd8fb728d502da768e16
c0a22001 <c0a2200113@edu.teu.ac.jp>
2023-11-21 16:33:51 +0900

    課題01

--- pygame_image.py
+++ pygame_image.py
@@ -7,21 +7,27 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習1背景画像Surface生成
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/fig/3.png")#練習2こうかとんのSurface生成
     kk_img = pg.transform.flip(kk_img, True, False)#練習2左右反転
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]#練習3回転
+    kk_img2 = pg.transform.rotozoom(kk_img, 2.5, 1.0)
+    kk_img3 = pg.transform.rotozoom(kk_img, 5, 1.0)
+    kk_img4 = pg.transform.rotozoom(kk_img, 7.5, 1.0)
+    kk_img5 = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_imgs = [kk_img, kk_img2, kk_img3, kk_img4, kk_img5, kk_img4, kk_img3, kk_img2, kk_img]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])#練習4背景表示
-        screen.blit(bg_img, [1600-x, 0])#練習6動く背景画像
-        screen.blit(kk_imgs[tmr%2], [300,200])#練習5こうかとん表示
+        screen.blit(bg_img2, [1600-x, 0])#練習6動く背景画像
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(kk_imgs[tmr%9], [300,200])#練習5こうかとん表示
         pg.display.update()
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
